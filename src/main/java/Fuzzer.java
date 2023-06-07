2ba211a3e7fa86acd7efec46a71f740795bafd70
Luna Geens <s0192441@ad.ua.ac.be>
2023-06-07 19:03:55 +0200

    Not cleaned up code with log file and binary and text

--- src/main/java/Fuzzer.java
+++ src/main/java/Fuzzer.java
@@ -1,5 +1,7 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Fuzzer {
 
@@ -7,34 +9,67 @@ public class Fuzzer {
     private static final int MAX_ITERATIONS = 100;
     private static final long TIME_BUDGET_MS = 15 * 60000; // 15 Minutes
 
+    private static File logFile;
+
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
+        int exitCode0 = 0;
+        int exitCode10 = 0;
+        int exitCode1 = 0;
+        int exitCodeUnknown = 0;
 
         // How many times does a random file and sequence has to be created?
         for (int i = 0; i < MAX_ITERATIONS; i++) {
 
-            // Generate inputs
-            RandomMap randomMap = new RandomMap();
-            File mapFile = randomMap.getMapFile();
+            // Pick random file type
+            Random random = new Random();
+            boolean fileType = random.nextBoolean();
+
+            // Generate random inputs for the map file
+            File mapFile;
+            String mapFileType;
+            String identicalLineWidths = "N.A.";
+            if (fileType) {
+                mapFileType = "Binary";
+                RandomBinaryMap randomBinaryMap = new RandomBinaryMap();
+                mapFile = randomBinaryMap.getRandomBinaryFile();
+            } else {
+                mapFileType = "Text";
+                RandomTextMap randomTextMap = new RandomTextMap();
+                mapFile = randomTextMap.getMapFile();
+                identicalLineWidths = randomTextMap.getIdenticalLineWidths();
+            }
+
+            // Generate random action sequence
             RandomActionSequence randomActionSequence = new RandomActionSequence();
             String actionSequence = randomActionSequence.getActionSequence();
 
             // Execute Jpacman with the generated inputs
-            int exitCode = executeJpacman(mapFile.getPath(), actionSequence);
+            List results = executeJpacman(mapFile.getPath(), actionSequence);
+            int exitCode = (int) results.get(0);
+            String outputMessages = (String) results.get(1);
 
             // Check the exit code and handle accordingly
             if (exitCode == 0) {  // Normal termination
                 System.out.println("Program terminated normally.");
-            } else if (exitCode == 10) {  // Normal termination
-                System.out.println("Program rejected the input.");
-            } else if (exitCode == 1) {    // Crash
-                System.out.println("Program crashed.");
-            } else {  // Handle other exit codes
-                System.out.println("Unknown exit code: " + exitCode);
+                exitCode0 = exitCode0 + 1;
+                writeLogEntry(exitCode, outputMessages, mapFile, mapFileType, identicalLineWidths, actionSequence);
+                cleanup(mapFile, randomActionSequence);  // Clean-up file and string
+            } else {// if not terminated normally, write log
+
+                if (exitCode == 10) {  // Normal termination
+                    System.out.println("Program rejected the input.");
+                    exitCode10 = exitCode10 + 1;
+                } else if (exitCode == 1) {    // Crash
+                    System.out.println("Program crashed.");
+                    exitCode1 = exitCode1 + 1;
+                } else {  // Handle other exit codes
+                    System.out.println("Unknown exit code: " + exitCode);
+                    exitCodeUnknown = exitCodeUnknown + 1;
+                }
             }
 
-            // Clean-up file and string
-            cleanup(randomMap, randomActionSequence);
+
 
             // Check if time budget is exhausted
             long endTime = System.currentTimeMillis();
@@ -47,25 +82,87 @@ public class Fuzzer {
             if (i == MAX_ITERATIONS - 1){ System.out.println("Maximum iterations reached.");}
         }
 
-    }
-    private static int executeJpacman(String mapFile, String actionSequence) {
-        try {
-            Process process = new ProcessBuilder("java", "-jar", "jpacman-3.0.1.jar", mapFile, actionSequence).start(); // command
-            process.waitFor(); // wait for process to complete
-            return process.exitValue(); // obtain exit value
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-            return -1;
-        }
+        if (exitCode0 != 0){System.out.println("Program terminated normally " + exitCode0 + " time(s).");}
+        if (exitCode10 != 10){System.out.println("Program rejected the output " + exitCode10 + " time(s).");}
+        if (exitCode1 != 0){System.out.println("Program crashed " + exitCode1 + " time(s).");}
+        if (exitCodeUnknown != 0){System.out.println("Program exited with unknown code " + exitCodeUnknown + " time(s).");}
     }
 
-    private static void cleanup(RandomMap randomMap, RandomActionSequence randomActionSequence) {
-        randomMap.deleteMapFile();
-        randomActionSequence.deleteActionSequence();
+    private static List executeJpacman(String mapFile, String actionSequence) {
+        List results = new ArrayList<>();
+        ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", "jpacman-3.0.1.jar", mapFile, actionSequence);
+        processBuilder.redirectErrorStream(true);
+        try {
+            Process process = processBuilder.start(); // command
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            StringBuilder outputBuilder = new StringBuilder();
+            while ((line = reader.readLine()) != null){ outputBuilder.append(line).append("\n");}
+
+            int exitCode = process.waitFor(); // wait for process to complete
+            results.add(exitCode);
+
+            String outputMessages = null;
+            if (exitCode != 0){ // check if it is crash or rejection
+                outputMessages = outputBuilder.toString(); }
+            results.add(outputMessages);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            results.add(-1);
+            results.add("Exception during process building");
+        }
+        return results;
+    }
+
+    // TODO fix dat dit terug werkt met methods per klasse
+    private static void cleanup(File mapFile, RandomActionSequence randomActionSequence) {
+        mapFile.delete();
         // Clean up any temporary files or resources used during the fuzzing process
         // For example, you can delete any generated map files that are no longer needed
         // You can use the File.delete() method to delete the files
         // Example: new File("path/to/file.map").delete();
     }
 
+    public static File getLogFile(){
+        if (logFile == null){ logFile = createLogFile(); }
+        return logFile;
+    }
+
+    public static File createLogFile() {
+        try {
+            // tODO Configs logdirecotry
+            File logDirectory =new File("C:\\ST\\JPacmanFuzz");
+            File logFile = File.createTempFile("log_", ".txt", logDirectory);
+            Fuzzer.logFile = logFile;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Fuzzer.logFile;
+    }
+
+
+    public static void writeLogEntry(int exitCode, String outputMessages, File mapFile, String mapFileType, String identicalLineWidths, String actionSequence){
+        File logFile = getLogFile();
+        try {
+            // Write the information to the log file
+            BufferedWriter writer = new BufferedWriter(new FileWriter(logFile));
+            writer.write("----------------------------------------------------");
+            writer.write("Exit Code: " + exitCode + "\n");
+            writer.write("Output Messages:\n");
+            writer.write(outputMessages);
+            writer.write("Map File path: " + mapFile.getAbsolutePath() + "\n");
+            writer.write("Map File type: " + mapFileType);
+            writer.write("Map File had identical line widths: " + identicalLineWidths);
+            writer.write("Action Sequence: " + actionSequence + "\n");
+
+            writer.close();
+
+            System.out.println("Log entry saved: " + logFile.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
--- src/main/java/RandomTextMap.java
+++ src/main/java/RandomTextMap.java
@@ -3,9 +3,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
 
-public class RandomMap {
-
-    // TODO Random max?
+public class RandomTextMap {
     private static final int MAX_MAP_WIDTH = 20;
     private static final int MAX_MAP_HEIGHT = 20;
 
@@ -24,7 +22,7 @@ public class RandomMap {
     /**
      * Makes a Random Map File.
      */
-    public RandomMap() {
+    public RandomTextMap() {
 
         Random random = new Random();
 
@@ -35,15 +33,18 @@ public class RandomMap {
         char[][] map = new char[MAP_WIDTH][MAP_HEIGHT];
 
         // Populate the map with random elements
-        // TODO Set wall random as well. Rows and col starts at 0!
         for (int row = 0; row < MAP_WIDTH; row++) {
             for (int col = 0; col < MAP_HEIGHT; col++) {
                 // Set empty cell, wall, monster, or Pacman randomly
                 int rand = random.nextInt(4); // 0 (included), 1, 2 and 3 (4 not included)
-                    if (rand < 1){ map[row][col] = 'W';
-                    } else if (rand < 2) { map[row][col] = 'M'; // Monster
-                    } else if (rand < 3) { map[row][col] = 'P'; // Pacman - Can only be one pacman?
-                    } else {  map[row][col] = '0'; // Empty cell
+                if (rand < 1) {
+                    map[row][col] = 'W';
+                } else if (rand < 2) {
+                    map[row][col] = 'M'; // Monster
+                } else if (rand < 3) {
+                    map[row][col] = 'P'; // Pacman - Can only be one pacman?
+                } else {
+                    map[row][col] = '0'; // Empty cell
                 }
             }
         }
@@ -55,7 +56,6 @@ public class RandomMap {
      * After executing this method (successfully or not), the boolean mapFileCreated is set to true.
      */
     private void createMapFile() {
-
         // Save the map to a file
         try {
             File mapFile = File.createTempFile("map", ".map");
@@ -79,18 +79,14 @@ public class RandomMap {
      * This way only one mapFile per instance.
      */
     public File getMapFile() {
-        if (!mapFileCreated){createMapFile();}         // If map isn't saved yet to variable:
+        if (!mapFileCreated) {
+            createMapFile();
+        }         // If map isn't saved yet to variable:
         return mapFile;
     }
 
-    // TODO Clean-up here?
-    /**
-     * Delete map file.
-     */
-    public void deleteMapFile(){
-        boolean successFullyDeleted = mapFile.delete();
-        if (!successFullyDeleted){System.out.println("Map file not successfully deleted.");}
-        this.mapFileDeleted = true; // true even if not successfully deleted
+    public String getIdenticalLineWidths() {
+      if (MAP_HEIGHT == MAP_WIDTH){ return "True"; }
+      else return "False";
     }
-
 }
