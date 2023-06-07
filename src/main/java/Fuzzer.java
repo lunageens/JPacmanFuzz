e76e5c3bd56ee4d911070f35b344b1061abe396d
Luna Geens <s0192441@ad.ua.ac.be>
2023-06-07 17:19:32 +0200

    Fuzzing JPacman: output on console and only valid characters

--- src/main/java/Fuzzer.java
+++ src/main/java/Fuzzer.java
@@ -1,2 +1,71 @@
-package PACKAGE_NAME;public class Fuzzer {
+import java.io.File;
+import java.io.IOException;
+
+public class Fuzzer {
+
+    // TODO configurations and reader?
+    private static final int MAX_ITERATIONS = 100;
+    private static final long TIME_BUDGET_MS = 15 * 60000; // 15 Minutes
+
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+        // How many times does a random file and sequence has to be created?
+        for (int i = 0; i < MAX_ITERATIONS; i++) {
+
+            // Generate inputs
+            RandomMap randomMap = new RandomMap();
+            File mapFile = randomMap.getMapFile();
+            RandomActionSequence randomActionSequence = new RandomActionSequence();
+            String actionSequence = randomActionSequence.getActionSequence();
+
+            // Execute Jpacman with the generated inputs
+            int exitCode = executeJpacman(mapFile.getPath(), actionSequence);
+
+            // Check the exit code and handle accordingly
+            if (exitCode == 0) {  // Normal termination
+                System.out.println("Program terminated normally.");
+            } else if (exitCode == 10) {  // Normal termination
+                System.out.println("Program rejected the input.");
+            } else if (exitCode == 1) {    // Crash
+                System.out.println("Program crashed.");
+            } else {  // Handle other exit codes
+                System.out.println("Unknown exit code: " + exitCode);
+            }
+
+            // Clean-up file and string
+            cleanup(randomMap, randomActionSequence);
+
+            // Check if time budget is exhausted
+            long endTime = System.currentTimeMillis();
+            long elapsedTime = endTime - startTime;
+            if (elapsedTime >= TIME_BUDGET_MS) {
+                System.out.println("Time limit reached.");
+                break;
+            }
+
+            if (i == MAX_ITERATIONS - 1){ System.out.println("Maximum iterations reached.");}
+        }
+
+    }
+    private static int executeJpacman(String mapFile, String actionSequence) {
+        try {
+            Process process = new ProcessBuilder("java", "-jar", "jpacman-3.0.1.jar", mapFile, actionSequence).start(); // command
+            process.waitFor(); // wait for process to complete
+            return process.exitValue(); // obtain exit value
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    private static void cleanup(RandomMap randomMap, RandomActionSequence randomActionSequence) {
+        randomMap.deleteMapFile();
+        randomActionSequence.deleteActionSequence();
+        // Clean up any temporary files or resources used during the fuzzing process
+        // For example, you can delete any generated map files that are no longer needed
+        // You can use the File.delete() method to delete the files
+        // Example: new File("path/to/file.map").delete();
+    }
+
 }
--- src/main/java/RandomMap.java
+++ src/main/java/RandomMap.java
@@ -1,2 +1,96 @@
-package PACKAGE_NAME;public class RandomMap {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class RandomMap {
+
+    // TODO Random max?
+    private static final int MAX_MAP_WIDTH = 20;
+    private static final int MAX_MAP_HEIGHT = 20;
+
+
+    private int MAP_WIDTH;
+    private int MAP_HEIGHT;
+
+    private char[][] map;
+
+    private File mapFile;
+
+    public boolean mapFileCreated = false;
+    public boolean mapFileDeleted = false;
+
+
+    /**
+     * Makes a Random Map File.
+     */
+    public RandomMap() {
+
+        Random random = new Random();
+
+        // TODO Check with 0 index and so on
+        this.MAP_HEIGHT = random.nextInt(MAX_MAP_HEIGHT);
+        this.MAP_WIDTH = random.nextInt(MAX_MAP_WIDTH);
+
+        char[][] map = new char[MAP_WIDTH][MAP_HEIGHT];
+
+        // Populate the map with random elements
+        // TODO Set wall random as well. Rows and col starts at 0!
+        for (int row = 0; row < MAP_WIDTH; row++) {
+            for (int col = 0; col < MAP_HEIGHT; col++) {
+                // Set empty cell, wall, monster, or Pacman randomly
+                int rand = random.nextInt(4); // 0 (included), 1, 2 and 3 (4 not included)
+                    if (rand < 1){ map[row][col] = 'W';
+                    } else if (rand < 2) { map[row][col] = 'M'; // Monster
+                    } else if (rand < 3) { map[row][col] = 'P'; // Pacman - Can only be one pacman?
+                    } else {  map[row][col] = '0'; // Empty cell
+                }
+            }
+        }
+        this.map = map;
+    }
+
+    /**
+     * Create file with random map. Set this as class variable. Class variable in null when error occurred.
+     * After executing this method (successfully or not), the boolean mapFileCreated is set to true.
+     */
+    private void createMapFile() {
+
+        // Save the map to a file
+        try {
+            File mapFile = File.createTempFile("map", ".map");
+            FileWriter writer = new FileWriter(mapFile);
+            for (int row = 0; row < MAP_WIDTH; row++) {
+                writer.write(map[row]);
+                writer.write(System.lineSeparator());
+            }
+            writer.close();
+            this.mapFile = mapFile;
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Map-file not successfully created.");
+            this.mapFile = null;
+        }
+        this.mapFileCreated = true; // true even if not successfully created
+    }
+
+    /**
+     * Get map file.
+     * This way only one mapFile per instance.
+     */
+    public File getMapFile() {
+        if (!mapFileCreated){createMapFile();}         // If map isn't saved yet to variable:
+        return mapFile;
+    }
+
+    // TODO Clean-up here?
+    /**
+     * Delete map file.
+     */
+    public void deleteMapFile(){
+        boolean successFullyDeleted = mapFile.delete();
+        if (!successFullyDeleted){System.out.println("Map file not successfully deleted.");}
+        this.mapFileDeleted = true; // true even if not successfully deleted
+    }
+
 }
